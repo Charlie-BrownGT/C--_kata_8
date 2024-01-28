e7452fd9e3357d612c2cdbfdc00e3cdf9d0f5015
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2024-01-28 08:32:06 +0000

    update

--- sort_and_star.cc
+++ sort_and_star.cc
@@ -1,5 +1,23 @@
+#include <algorithm>
+#include <string>
 #include <vector>
 
-std::vector<int> pipe_fix(const std::vector<int>& nums) {
-  // your code here
+using namespace std;
+
+string twoSort(vector<string> s)
+{
+    sort(s.begin(), s.end());
+
+    string str = s[0];
+    string answer;
+
+    for(char &s: str)
+    {
+        answer.push_back(s);
+        answer.push_back('*');
+        answer.push_back('*');
+        answer.push_back('*');
+    }
+    
+    return answer.substr(0, answer.length() - 3);
 }
